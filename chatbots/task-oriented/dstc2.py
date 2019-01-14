8dd99761cd64053aa3dcca06199e7d113322c951
Mansour Saffar M <msaffarmehrjardy@gmail.com>
2019-01-14 13:15:49 -0700

    modified dstc2 problem

--- chatbots/task-oriented/dstc2.py
+++ chatbots/task-oriented/dstc2.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Data generators for SquaAD (https://rajpurkar.github.io/SQuAD-explorer/).
+"""Data generators for DSTCS2 challenge.
 """
 import json
 import os
