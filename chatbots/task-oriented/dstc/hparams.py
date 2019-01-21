7809432a05aabbd6ea912c653c07564d357b8b49
Mansour Saffar M <msaffarmehrjardy@gmail.com>
2019-01-21 16:39:53 -0700

    adde bilstm params

--- chatbots/task-oriented/dstc/hparams.py
+++ chatbots/task-oriented/dstc/hparams.py
@@ -161,95 +161,164 @@ def dstc_lstm_attention_hparams_v4():
 
 
 
-# ########################################## UNIVERSAL TRANSFORMER #################################3
-# from tensor2tensor.models.transformer import transformer_tiny
 
 
-# # dropout = [0.2,0.7]
-# # layers = [2,4]
-# # attention heads = [1,4]
+########################################## BIDIRECTTIONAL LSTM #################################
+from tensor2tensor.models.lstm import lstm_seq2seq, lstm_attention_base
+# dropout = [0.2,0.7]
+# layers = [2,4]
+@registry.register_hparams
+def dstc_bilstm_hparams_v1():
+    hparams = lstm_seq2seq()
+    hparams.num_hidden_layers = 2
+    hparams.dropout=0.2
+    return hparams
 
-# @registry.register_hparams
-# def dstc_transformer_hparams_v1():
-#     hparams = transformer_tiny()
-#     hparams.num_hidden_layers = 2
-#     hparams.dropout=0.2
-#     hparams.hidden_size = 128
-#     hparams.filter_size = 512
-#     hparams.num_heads = 1
-#     return hparams
-
-# @registry.register_hparams
-# def dstc_transformer_hparams_v2():
-#     hparams = transformer_tiny()
-#     hparams.num_hidden_layers = 2
-#     hparams.dropout=0.2
-#     hparams.hidden_size = 128
-#     hparams.filter_size = 512
-#     hparams.num_heads = 4
-#     return hparams
-
-# @registry.register_hparams
-# def dstc_transformer_hparams_v3():
-#     hparams = transformer_tiny()
-#     hparams.num_hidden_layers = 4
-#     hparams.dropout=0.2
-#     hparams.hidden_size = 128
-#     hparams.filter_size = 512
-#     hparams.num_heads = 1
-#     return hparams
-
-# @registry.register_hparams
-# def dstc_transformer_hparams_v4():
-#     hparams = transformer_tiny()
-#     hparams.num_hidden_layers = 4
-#     hparams.dropout=0.2
-#     hparams.hidden_size = 128
-#     hparams.filter_size = 512
-#     hparams.num_heads = 4
-#     return hparams
-
-# @registry.register_hparams
-# def dstc_transformer_hparams_v5():
-#     hparams = transformer_tiny()
-#     hparams.num_hidden_layers = 2
-#     hparams.dropout=0.7
-#     hparams.hidden_size = 128
-#     hparams.filter_size = 512
-#     hparams.num_heads = 1
-#     return hparams
-
-# @registry.register_hparams
-# def dstc_transformer_hparams_v6():
-#     hparams = transformer_tiny()
-#     hparams.num_hidden_layers = 2
-#     hparams.dropout=0.7
-#     hparams.hidden_size = 128
-#     hparams.filter_size = 512
-#     hparams.num_heads = 4
-#     return hparams
-
-# @registry.register_hparams
-# def dstc_transformer_hparams_v7():
-#     hparams = transformer_tiny()
-#     hparams.num_hidden_layers = 4
-#     hparams.dropout=0.7
-#     hparams.hidden_size = 128
-#     hparams.filter_size = 512
-#     hparams.num_heads = 1
-#     return hparams
-
-# @registry.register_hparams
-# def dstc_transformer_hparams_v8():
-#     hparams = transformer_tiny()
-#     hparams.num_hidden_layers = 4
-#     hparams.dropout=0.7
-#     hparams.hidden_size = 128
-#     hparams.filter_size = 512
-#     hparams.num_heads = 4
-#     return hparams
+@registry.register_hparams
+def dstc_bilstm_hparams_v2():
+    hparams = lstm_seq2seq()
+    hparams.num_hidden_layers = 2
+    hparams.dropout=0.7
+    return hparams
 
 
+@registry.register_hparams
+def dstc_bilstm_hparams_v3():
+    hparams = lstm_seq2seq()
+    hparams.num_hidden_layers = 4
+    hparams.dropout=0.2
+    return hparams
+
+@registry.register_hparams
+def dstc_bilstm_hparams_v4():
+    hparams = lstm_seq2seq()
+    hparams.num_hidden_layers = 4
+    hparams.dropout=0.7
+    return hparams
+
+
+# attention LSTM
+@registry.register_hparams
+def dstc_bilstm_attention_hparams_v1():
+    hparams = lstm_attention_base()
+    hparams.num_hidden_layers = 2
+    hparams.dropout=0.2
+    return hparams
+
+
+# attention LSTM
+@registry.register_hparams
+def dstc_bilstm_attention_hparams_v2():
+    hparams = lstm_attention_base()
+    hparams.num_hidden_layers = 2
+    hparams.dropout=0.7
+    return hparams
+
+# attention LSTM
+@registry.register_hparams
+def dstc_bilstm_attention_hparams_v3():
+    hparams = lstm_attention_base()
+    hparams.num_hidden_layers = 4
+    hparams.dropout=0.2
+    return hparams
+
+# attention LSTM
+@registry.register_hparams
+def dstc_bilstm_attention_hparams_v4():
+    hparams = lstm_attention_base()
+    hparams.num_hidden_layers = 4
+    hparams.dropout=0.7
+    return hparams
+
+
+
+########################################## UNIVERSAL TRANSFORMER #################################3
+from tensor2tensor.models.research.universal_transformer import universal_transformer_tiny
+
+
+# dropout = [0.2,0.7]
+# layers = [2,4]
+# attention heads = [1,4]
+
+@registry.register_hparams
+def dstc_universal_transformer_hparams_v1():
+    hparams = universal_transformer_tiny()
+    hparams.num_hidden_layers = 2
+    hparams.dropout=0.2
+    hparams.hidden_size = 128
+    hparams.filter_size = 512
+    hparams.num_heads = 1
+    return hparams
+
+@registry.register_hparams
+def dstc_universal_transformer_hparams_v2():
+    hparams = universal_transformer_tiny()
+    hparams.num_hidden_layers = 2
+    hparams.dropout=0.2
+    hparams.hidden_size = 128
+    hparams.filter_size = 512
+    hparams.num_heads = 4
+    return hparams
+
+@registry.register_hparams
+def dstc_universal_transformer_hparams_v3():
+    hparams = universal_transformer_tiny()
+    hparams.num_hidden_layers = 4
+    hparams.dropout=0.2
+    hparams.hidden_size = 128
+    hparams.filter_size = 512
+    hparams.num_heads = 1
+    return hparams
+
+@registry.register_hparams
+def dstc_universal_transformer_hparams_v4():
+    hparams = universal_transformer_tiny()
+    hparams.num_hidden_layers = 4
+    hparams.dropout=0.2
+    hparams.hidden_size = 128
+    hparams.filter_size = 512
+    hparams.num_heads = 4
+    return hparams
+
+@registry.register_hparams
+def dstc_universal_transformer_hparams_v5():
+    hparams = universal_transformer_tiny()
+    hparams.num_hidden_layers = 2
+    hparams.dropout=0.7
+    hparams.hidden_size = 128
+    hparams.filter_size = 512
+    hparams.num_heads = 1
+    return hparams
+
+@registry.register_hparams
+def dstc_universal_transformer_hparams_v6():
+    hparams = universal_transformer_tiny()
+    hparams.num_hidden_layers = 2
+    hparams.dropout=0.7
+    hparams.hidden_size = 128
+    hparams.filter_size = 512
+    hparams.num_heads = 4
+    return hparams
+
+@registry.register_hparams
+def dstc_universal_transformer_hparams_v7():
+    hparams = universal_transformer_tiny()
+    hparams.num_hidden_layers = 4
+    hparams.dropout=0.7
+    hparams.hidden_size = 128
+    hparams.filter_size = 512
+    hparams.num_heads = 1
+    return hparams
+
+@registry.register_hparams
+def dstc_universal_transformer_hparams_v8():
+    hparams = universal_transformer_tiny()
+    hparams.num_hidden_layers = 4
+    hparams.dropout=0.7
+    hparams.hidden_size = 128
+    hparams.filter_size = 512
+    hparams.num_heads = 4
+    return hparams
 
 
 
