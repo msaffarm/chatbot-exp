b8070d23bbfdf6dee79feaec5f063b1b31c2983a
mansour <msaffarmehrjardy@gmail.com>
2019-05-08 08:25:44 -0600

    Modify run-train to include all models

--- chatbots/task-oriented/dstc/run-train.sh
+++ chatbots/task-oriented/dstc/run-train.sh
@@ -71,3 +71,232 @@ do
     --keep_checkpoint_max=20
 
 done
+
+
+# ############################################################################################
+
+MODEL=lstm_seq2seq
+declare -a hparams=("dstc_lstm_hparams_v1"
+                    "dstc_lstm_hparams_v2"
+                    "dstc_lstm_hparams_v3"
+                    "dstc_lstm_hparams_v4"
+                    )
+
+CURRENT_DIR=$PWD
+DATA_DIR=$CURRENT_DIR/t2t_data
+TMP_DIR=$CURRENT_DIR/t2t_datagen
+TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+
+# Train loop
+for hparam in "${hparams[@]}"
+do
+  echo "$hparam"
+  HPARAMS=$hparam
+  TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+  mkdir -p $TRAIN_DIR
+  
+  t2t-trainer \
+    --t2t_usr_dir=$CURRENT_DIR \
+    --data_dir=$DATA_DIR \
+    --problem=$PROBLEM \
+    --model=$MODEL \
+    --hparams_set=$HPARAMS \
+    --output_dir=$TRAIN_DIR \
+    --train_steps=6000 \
+    --eval_steps=999999 \
+    --local_eval_frequency=300 \
+    --eval_throttle_seconds=1 \
+    --keep_checkpoint_max=20
+
+done
+
+##########################################################################
+
+MODEL=lstm_seq2seq_attention
+declare -a hparams=("dstc_lstm_attention_hparams_v1"
+                    "dstc_lstm_attention_hparams_v2"
+                    "dstc_lstm_attention_hparams_v3"
+                    "dstc_lstm_attention_hparams_v4"
+                    )
+
+CURRENT_DIR=$PWD
+DATA_DIR=$CURRENT_DIR/t2t_data
+TMP_DIR=$CURRENT_DIR/t2t_datagen
+TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+
+# Train loop
+for hparam in "${hparams[@]}"
+do
+  echo "$hparam"
+  HPARAMS=$hparam
+  TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+  mkdir -p $TRAIN_DIR
+  
+  t2t-trainer \
+    --t2t_usr_dir=$CURRENT_DIR \
+    --data_dir=$DATA_DIR \
+    --problem=$PROBLEM \
+    --model=$MODEL \
+    --hparams_set=$HPARAMS \
+    --output_dir=$TRAIN_DIR \
+    --train_steps=6000 \
+    --eval_steps=999999 \
+    --local_eval_frequency=300 \
+    --eval_throttle_seconds=1 \
+    --keep_checkpoint_max=20
+
+done
+
+#########################################################
+MODEL=lstm_seq2seq_bidirectional_encoder
+declare -a hparams=("dstc_bilstm_hparams_v1"
+                    "dstc_bilstm_hparams_v2"
+                    "dstc_bilstm_hparams_v3"
+                    "dstc_bilstm_hparams_v4"
+                    )
+
+CURRENT_DIR=$PWD
+DATA_DIR=$CURRENT_DIR/t2t_data
+TMP_DIR=$CURRENT_DIR/t2t_datagen
+TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+
+# Train loop
+for hparam in "${hparams[@]}"
+do
+  echo "$hparam"
+  HPARAMS=$hparam
+  TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+  mkdir -p $TRAIN_DIR
+  
+  t2t-trainer \
+    --t2t_usr_dir=$CURRENT_DIR \
+    --data_dir=$DATA_DIR \
+    --problem=$PROBLEM \
+    --model=$MODEL \
+    --hparams_set=$HPARAMS \
+    --output_dir=$TRAIN_DIR \
+    --train_steps=6000 \
+    --eval_steps=999999 \
+    --local_eval_frequency=300 \
+    --eval_throttle_seconds=1 \
+    --keep_checkpoint_max=20
+
+done
+
+##########################################################################
+
+MODEL=lstm_seq2seq_attention_bidirectional_encoder
+declare -a hparams=("dstc_bilstm_attention_hparams_v1"
+                    "dstc_bilstm_attention_hparams_v2"
+                    "dstc_bilstm_attention_hparams_v3"
+                    "dstc_bilstm_attention_hparams_v4"
+                    )
+
+CURRENT_DIR=$PWD
+DATA_DIR=$CURRENT_DIR/t2t_data
+TMP_DIR=$CURRENT_DIR/t2t_datagen
+TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+
+# Train loop
+for hparam in "${hparams[@]}"
+do
+  echo "$hparam"
+  HPARAMS=$hparam
+  TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+  mkdir -p $TRAIN_DIR
+  
+  t2t-trainer \
+    --t2t_usr_dir=$CURRENT_DIR \
+    --data_dir=$DATA_DIR \
+    --problem=$PROBLEM \
+    --model=$MODEL \
+    --hparams_set=$HPARAMS \
+    --output_dir=$TRAIN_DIR \
+    --train_steps=6000 \
+    --eval_steps=999999 \
+    --local_eval_frequency=300 \
+    --eval_throttle_seconds=1 \
+    --keep_checkpoint_max=20
+
+done
+
+#####################################################################################
+MODEL=universal_transformer
+declare -a hparams=("dstc_universal_transformer_hparams_v1"
+                    "dstc_universal_transformer_hparams_v2"
+                    "dstc_universal_transformer_hparams_v3"
+                    "dstc_universal_transformer_hparams_v4"
+                    "dstc_universal_transformer_hparams_v5"
+                    "dstc_universal_transformer_hparams_v6"
+                    "dstc_universal_transformer_hparams_v7"
+                    "dstc_universal_transformer_hparams_v8"
+                    )
+
+CURRENT_DIR=$PWD
+DATA_DIR=$CURRENT_DIR/t2t_data
+TMP_DIR=$CURRENT_DIR/t2t_datagen
+TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+
+# Train loop
+for hparam in "${hparams[@]}"
+do
+  echo "$hparam"
+  HPARAMS=$hparam
+  TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+  mkdir -p $TRAIN_DIR
+  
+  t2t-trainer \
+    --t2t_usr_dir=$CURRENT_DIR \
+    --data_dir=$DATA_DIR \
+    --problem=$PROBLEM \
+    --model=$MODEL \
+    --hparams_set=$HPARAMS \
+    --output_dir=$TRAIN_DIR \
+    --train_steps=6000 \
+    --eval_steps=999999 \
+    --local_eval_frequency=300 \
+    --eval_throttle_seconds=1 \
+    --keep_checkpoint_max=20
+
+done
+
+##########################################################################
+
+MODEL=universal_transformer
+declare -a hparams=("dstc_universal_transformer_act_hparams_v1"
+                    "dstc_universal_transformer_act_hparams_v2"
+                    "dstc_universal_transformer_act_hparams_v3"
+                    "dstc_universal_transformer_act_hparams_v4"
+                    "dstc_universal_transformer_act_hparams_v5"
+                    "dstc_universal_transformer_act_hparams_v6"
+                    "dstc_universal_transformer_act_hparams_v7"
+                    "dstc_universal_transformer_act_hparams_v8"
+                    )
+
+CURRENT_DIR=$PWD
+DATA_DIR=$CURRENT_DIR/t2t_data
+TMP_DIR=$CURRENT_DIR/t2t_datagen
+TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+
+# Train loop
+for hparam in "${hparams[@]}"
+do
+  echo "$hparam"
+  HPARAMS=$hparam
+  TRAIN_DIR=$CURRENT_DIR/t2t_train/$MODEL-$HPARAMS
+  mkdir -p $TRAIN_DIR
+  
+  t2t-trainer \
+    --t2t_usr_dir=$CURRENT_DIR \
+    --data_dir=$DATA_DIR \
+    --problem=$PROBLEM \
+    --model=$MODEL \
+    --hparams_set=$HPARAMS \
+    --output_dir=$TRAIN_DIR \
+    --train_steps=6000 \
+    --eval_steps=999999 \
+    --local_eval_frequency=300 \
+    --eval_throttle_seconds=1 \
+    --keep_checkpoint_max=20
+
+done
