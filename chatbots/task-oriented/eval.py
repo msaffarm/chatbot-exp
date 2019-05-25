6a886cc50119d38ef1e47f8126edc41dfb03d773
mansour <msaffarmehrjardy@gmail.com>
2019-05-25 16:03:26 -0600

    update eval

--- chatbots/task-oriented/eval.py
+++ chatbots/task-oriented/eval.py
@@ -4,16 +4,32 @@ CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 UTILS_DIR = os.path.join(CURRENT_DIR,"../../utils")
 sys.path.append(UTILS_DIR)
 from evaluator import Evaluator
+import argparse
 
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser()
+
+    # File arguments
+    parser.add_argument('--target_path', dest='target_file', help='Path to target file')
+    parser.add_argument('--response_path', dest='response_file', help='Path to response file')
+    parser.add_argument('--diagid_path', dest='diagid_file', help='Path to diagid file')
+    parser.add_argument('--entity_path', dest='entity_file', help='Path to diag entities')
+
+    return parser.parse_args()
 
 def main():
-    response_file = "Movie-response.txt"
-    target_file = "Movie-target-test.txt"
+    args = parse_arguments()
+    target_file = args.target_file
+    response_file = args.response_file
+    diagid_file = args.diagid_file
+    entity_file = args.entity_file
 
     eval = Evaluator()
-    eval.read_data_from_files(os.path.join(CURRENT_DIR,target_file),os.path.join(CURRENT_DIR,response_file))
+    eval.read_data_from_files(target_file,response_file,diagid_file,entity_file)
     
-    report = eval.get_eval_stats()
+    report = eval.get_eval_stats(mode='hard')
     print(report)
 
 
